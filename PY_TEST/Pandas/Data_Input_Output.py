e36da7d4918ad153c7dec744cad8628b98936dce
RRDD <RRDD@RRDD-PC>
2018-05-31 17:07:45 -0400

    DD_THIRD

--- PY_TEST/Pandas/Data_Input_Output.py
+++ PY_TEST/Pandas/Data_Input_Output.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
-print(pd.read_csv('example.csv'))
+df = pd.read_csv('example.csv')
+print(df)
 
-#print(pd.read_excel('Excel_Sample'))
+df['NEW'] = [500, 501, 502, 503]
+df.to_csv('newEx.csv', index=False)
+
+df1 = pd.read_excel('Excel_Sample.xlsx',sheet_name='Sheet1')
+print(df1)
+
+#df1['WOW'] = [16, 17, 18, 19]
+#df1.to_excel('Excel_Sample.xlsx', sheet_name='NEWSHEET')
+
+data = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
+print("LENGTH: ",len(data))
---
+++ PY_TEST/Pandas/SF_Salaries.py
@@ -0,0 +1,1 @@
+print('Dipen Desai')
