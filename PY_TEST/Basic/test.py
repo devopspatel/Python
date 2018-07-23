f1b0351f8b86af1c0e9704a50a63960639a6b01b
RRDD <RRDD@RRDD-PC>
2018-07-23 15:35:05 -0400

    aa

--- PY_TEST/Basic/test.py
+++ PY_TEST/Basic/test.py
@@ -1,22 +1,21 @@
 import datetime
 
 start = datetime.datetime.now()
-
-n = 310
 nl = []
-
 for i in range(1, 3000001):
     if (i%2 == 0):
         nl.append(i)
 
-# print (f'EVENs >> {nl}')
 end = datetime.datetime.now()
-print (f'TIME DELTA: {end - start}')
+print (f'1 TIME DELTA: {end - start}')
 
 
 start = datetime.datetime.now()
-# nl = list(i for i in range(1,1000001) if i%2 == 0)
-nl = [i for i in range(1,3000001) if i%2 == 0]
-# print (f'2 EVENs >> {nl}')
+nl = list(i for i in range(1,3000001) if i%2 == 0)
 end = datetime.datetime.now()
 print (f'2 TIME DELTA: {end - start}')
+
+start = datetime.datetime.now()
+nl = [i for i in range(1,3000001) if i%2 == 0]
+end = datetime.datetime.now()
+print (f'3 TIME DELTA: {end - start}')
