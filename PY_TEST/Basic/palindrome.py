8d51e015dba8911ab07a05f5790811d43bfa5537
dd <dd@localhost.localdomain>
2019-02-03 03:02:51 -0500

    One Liner

--- PY_TEST/Basic/palindrome.py
+++ PY_TEST/Basic/palindrome.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        b = str(i)
-        br = b[::-1]
-        return True if (b == br) else False
-        # return True if (str(x) == str(x)[::-1]) else False
+        # b = str(i)
+        # br = b[::-1]
+        # return True if (b == br) else False
+        return True if (str(x) == str(x)[::-1]) else False
 
 i = 15544551
 x = Solution()
