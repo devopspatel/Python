bfcdd73c49db1b29f7537af28aec334498405f64
Dipen Desai <dnd.devops@gmail.com>
2019-02-03 00:01:39 -0800

    Palindrome with Logic

---
+++ PY_TEST/Basic/palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        b = str(i)
+        br = b[::-1]
+        return True if (b == br) else False
+        # return True if (str(x) == str(x)[::-1]) else False
+
+i = 15544551
+x = Solution()
+print (x.isPalindrome(i))
