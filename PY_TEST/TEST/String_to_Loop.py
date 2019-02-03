d2390532ab851ed9df3a8a8fe530d493a660b7a0
Dipen Desai <dnd.devops@gmail.com>
2019-02-02 23:36:50 -0800

    String to Loop 2.7.5

--- PY_TEST/TEST/String_to_Loop.py
+++ PY_TEST/TEST/String_to_Loop.py
@@ -7,25 +7,29 @@ class Solution(object):
         """
         if (len(s) > 0):
             l = [['0' for _ in range(len(s))] for __ in range(numRows)]
-        
+
         i = 0
         rowInv = 0
         cc = 0
-        
+
         while (cc < len(s) and len(s) > 0 and len(s) > numRows > 1):
-            print (f'WHILE LOOP: CC: {cc} >> S[cc]: {s[cc]}')
-             
+            # print (f'WHILE LOOP: CC: {cc} >> S[cc]: {s[cc]}')
+            # print ('WHILE LOOP: CC: {0} >> S[cc]: {1}'.format(cc, s[cc]))
+
             for j in range(numRows):
                 if (cc < len(s)):
-                    print (f'J: {j} >> FOR LOOP: CC: {cc} >> S[cc]: {s[cc]}')
+                    # print (f'J: {j} >> FOR LOOP: CC: {cc} >> S[cc]: {s[cc]}')
+                    # print ('J: {0} >> FOR LOOP: CC: {1} >> S[cc]: {2}'.format(j, cc, s[cc]))
                     if (i == 0 or (i%(numRows-1) == 0 and rowInv == 0)):
-                        print (f'IF >>>>>> I: {i} >> J: {j} >> CC: {cc} >> S[cc]: {s[cc]}')
+                        # print (f'IF >>>>>> I: {i} >> J: {j} >> CC: {cc} >> S[cc]: {s[cc]}')
+                        # print ('IF >>>>>> I: {0} >> J: {1} >> CC: {2} >> S[cc]: {3}'.format(i, j, cc, s[cc]))
                         l[j][i] = s[cc]
                         cc += 1
                         if (j == (numRows - 1)):
                             rowInv = numRows - 2
                     elif (rowInv > 0):
-                        print (f'ELSE >>>>>> I: {i} >> J: {j} >> CC: {cc} >> S[cc]: {s[cc]}')
+                        # print (f'ELSE >>>>>> I: {i} >> J: {j} >> CC: {cc} >> S[cc]: {s[cc]}')
+                        # print ('ELSE >>>>>> I: {0} >> J: {1} >> CC: {2} >> S[cc]: {3}'.format(i, j, cc, s[cc]))
                         l[rowInv][i] = s[cc]
                         cc += 1
                         rowInv -= 1
@@ -33,7 +37,7 @@ class Solution(object):
                         break
             else:
                 i += 1
-        
+
         ret_str = ''
         if (len(s) > 1 and len(s) > numRows > 1):
             for j in range(numRows):
@@ -42,12 +46,11 @@ class Solution(object):
                         ret_str += l[j][i]
         elif (numRows == 1 or len(s) <= numRows):
             ret_str = s
-        
+
         return ret_str
-        
+
 s = Solution()
 # print(s.convert('ABCDEFGHIJKLMN', 4))
-# print(s.convert('PAYPALISHIRING', 3))
-print(s.convert('A', 2))
+print(s.convert('PAYPALISHIRING', 3))
+# print(s.convert('A', 2))
 # print(s.convert('AB', 1))
-
