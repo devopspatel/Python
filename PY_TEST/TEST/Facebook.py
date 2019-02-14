83b4f49abbe1920ca28db1663aa504625d968766
Dipen Desai <dnd.devops@gmail.com>
2019-02-14 14:34:22 -0800

    Adding FB Interview Questions

---
+++ PY_TEST/TEST/Facebook.py
@@ -0,0 +1,78 @@
+# PROBLEM 1
+
+"""
+1. Not Vovel > Rotate
+2. Vovel > Not Rotate
+3. add 'ma' at the end
+4. add 'a' depending upon number
+
+E.g. > 'I Like Goat Style' > Imaa ikeLmaaa oatGmaaaa tyleSmaaaa
+"""
+
+def word_rotate(st):
+    """ Word Rotate """
+    words = st.split(' ')
+
+    i = 1
+    for word in words:
+        if (word[0].lower() not in ['a', 'e', 'i', 'o', 'u']):
+            word = word[1:len(word)+1] + word[0]
+        words[i-1] = word + 'ma' + 'a'* i
+        i += 1
+    return (' ').join(words)
+
+print (word_rotate('I Like Goat Style'))
+print (word_rotate('My Name is Dipen Desai and yours Dhara Desai'))
+
+
+# PROBLEM 2
+"""
+1. Find coordinates for tuples
+2self.
+
+E.g.
+find_ship(grid) < hit_targer(x, y)
+
+Return > ((2, 3), (3, 3), (4, 3))
+
+. . . . . . . .
+. . X . . . . .
+. . X . . . . .
+. . X . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+"""
+
+def hit_location(x, y):
+    grid_setup = [['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', 'X', 'X', 'X', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.']]
+
+    return True if (grid_setup[x-1][y-1] == 'X') else False
+
+def find_ship(grid_size):
+
+    if (grid_size < 3):
+        return 'Not Valid Grid'
+
+    d = []
+    for i in range(1, grid_size + 1):
+        for j in range(1, grid_size + 1):
+            if (hit_location(i, j)):
+                d.append((i, j))
+                if (hit_location(i, j+1) and hit_location(i, j+2)):
+                    d.append((i, j+1))
+                    d.append((i, j+2))
+                else:
+                    d.append((i+1, j))
+                    d.append((i+2, j))
+                return tuple(d)
+
+print(find_ship(8))
