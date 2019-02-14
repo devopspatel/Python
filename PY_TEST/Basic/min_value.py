3345d8b8adfd6b65fb0e1757f53084c7d8604ce0
Dipen Desai <dnd.devops@gmail.com>
2019-02-14 09:40:29 -0800

    Min Value File Updates

--- PY_TEST/Basic/min_value.py
+++ PY_TEST/Basic/min_value.py
@@ -1,26 +1,35 @@
 import random
-import datetime
 
 # rl = list(random.randint(1, 30) for _ in range(10))
 rl = list(random.randint(1, 30000) for _ in range(10000))
-print (rl)
 
-start = datetime.datetime.now()
-min_val = rl[0]
-for i in range(len(rl)):
-    for j in range(len(rl)):
-        if (rl[i] < rl [j] and rl[i] < min_val):
+def time_it(func):
+    import datetime
+    def wrapper(*args, **kwargs):
+        start = datetime.datetime.now()
+        result = func(*args, **kwargs)
+        end = datetime.datetime.now()
+        print ('Functiona: {0} --- Time: {1}'.format(func.__name__,end-start))
+        return result
+    return wrapper
+
+@time_it
+def min_val1(rl):
+    min_val = rl[0]
+    for i in range(len(rl)):
+        for j in range(len(rl)):
+            if (rl[i] < rl [j] and rl[i] < min_val):
+                min_val = rl[i]
+    return min_val
+
+@time_it
+def min_val2(rl):
+    min_val = rl[0]
+    for i in range(1, len(rl) - 1):
+        if (rl[i] < rl[i + 1] and rl[i] < min_val):
             min_val = rl[i]
-end = datetime.datetime.now()
-
-print (f'1 --- FINAL MIN VAL: {min_val} >> TIME DELTA: {end - start}')
+    return min_val
 
 
-start = datetime.datetime.now()
-min_val = rl[0]
-
-for i in range(1, len(rl) - 1):
-    if (rl[i] < rl[i + 1] and rl[i] < min_val):
-        min_val = rl[i]
-end = datetime.datetime.now()
-print (f'2 --- FINAL MIN VAL: {min_val} >> TIME DELTA: {end - start}')
+print ('Min Value F1: {0}'.format(min_val1(rl)))
+print ('Min Value F2: {0}'.format(min_val2(rl)))
